0ea9387b741aeb26a50b3cdd645620c59dfbc9b2
Lolikarbuzik <73756261+xXlubimyarbuzyXx@users.noreply.github.com>
2022-06-04 11:04:29 +0200

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,10 @@
+return {
+    ["JetpackJumpers"] = 2899971524, -- working
+    --["PetFightersSimulator"] = 2902009413, -- working
+    ["AnimeFighters"] = 2324662457,
+    --["ProjectHero"] = 2209794754, -- support later
+    --["Legends_Re:Writen"] = 2270890905, -- support later
+    --["PetSimulatorX"] = 2316994223, -- support later
+    --["WeaponFightingSimulator"]=3262314006, -- support later
+    --["BladeQuest"]=2429242760, -- support later 
+}
