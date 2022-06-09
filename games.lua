33110d1b18385d12a81c82024efc455932b16eee
Lolikarbuzik <73756261+xXlubimyarbuzyXx@users.noreply.github.com>
2022-06-09 13:49:00 +0200

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,7 +1,7 @@
 return {
     ["JetpackJumpers"] = 2899971524, -- working
     --["PetFightersSimulator"] = 2902009413, -- working
-    ["AnimeFighters"] = 2324662457,
+    --["AnimeFighters"] = 2324662457,
     --["ProjectHero"] = 2209794754, -- support later
     --["Legends_Re:Writen"] = 2270890905, -- support later
     --["PetSimulatorX"] = 2316994223, -- support later
